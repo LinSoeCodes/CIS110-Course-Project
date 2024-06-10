eb2e83cab1adfa6c9b4207034806bc13a9566e72
username <useremail>
2024-06-09 20:57:13 -0700

    First Yes or No

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -33,9 +33,12 @@ while keepPlaying.lower() == "yes":
     print(f"{name} realized using {typeOfCommute} will get there the fastest.") 
     print(f"{name} is finally on the way to {country} taking {item} to help in the journey.") 
     print(f"{name} is dropped off in the remote jungle of {country}. there are a lot of trees and jungle is deep")
-    print(f"Even though the journey is tough, {name} doesn't give up. After many hours of exploring the jungle, {adventurerName} finds an old looking paper on the ground which looks like a map")
+    print(f"Even though the journey is tough, {name} doesn't give up. After many hours of exploring the jungle, {name} finds an old looking paper on the ground which looks like a map.")
     
-    takeTheMap
+    takeTheMap = input(f"Should {name} picks up the map? Type yes or no: ")
+    while takeTheMap.lower() != "yes" and takeTheMap.lower() != "no":
+        takeTheMap = input("Please type yes or no: ") 
+        
     
 
 
